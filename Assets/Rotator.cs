00ee5196eed1444abcf38c8dc9e420c43b333c84
Martin Leissler <martin@leissler.net>
2015-11-26 15:39:59 +0100

    Added a rotator

---
+++ Assets/Rotator.cs
@@ -0,0 +1,11 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Rotator : MonoBehaviour {
+	public float speed = 100;
+	
+	// Update is called once per frame
+	void Update () {
+		transform.Rotate (0, speed * Time.deltaTime, 0);
+	}
+}
